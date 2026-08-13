e13bb2aa3ab60f05128ce531bd1414f4c2070313
Red Elh <redaelhiri9@gmail.com>
2026-08-13 23:54:02 +0100

    fix: replace bcrypt with bcryptjs in automator.controller

--- src/backend/controllers/automator.controller.js
+++ src/backend/controllers/automator.controller.js
@@ -1,5 +1,5 @@
 import pool from '../config/db.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { checkAgentInactivityAndNotify } from '../services/inactivity.service.js';
 
 function rowToAutomator(row) {
